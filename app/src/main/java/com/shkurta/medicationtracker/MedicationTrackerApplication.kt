8b11800776b99567e1273ca3bcd22c227bc754c2
shkur <shkurtaklaudjo@gmail.com>
2026-01-08 20:30:41 +0200

    wip

--- app/src/main/java/com/shkurta/medicationtracker/MedicationTrackerApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.shkurta.medicationtracker
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MedicationTrackerApplication : Application()
